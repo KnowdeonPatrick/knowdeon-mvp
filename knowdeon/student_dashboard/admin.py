b8faa7f456458a24fbae05fc26f0115d14246876
Georgi <g.pachev@futuresoft.bg>
2018-10-12 18:00:42 +0300

    Work-on-version-1.0-requirements

--- knowdeon/student_dashboard/admin.py
+++ knowdeon/student_dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from student_dashboard.models import Course, Module, SubmitedCourse, Chapter, Section, Item
+from student_dashboard.models import Course, Module, SubmitedCourse, Chapter, Section, Item, Profile
 # Register your models here.
 
 admin.site.register(SubmitedCourse)
@@ -8,3 +8,4 @@ admin.site.register(Module)
 admin.site.register(Chapter)
 admin.site.register(Section)
 admin.site.register(Item)
+admin.site.register(Profile)
